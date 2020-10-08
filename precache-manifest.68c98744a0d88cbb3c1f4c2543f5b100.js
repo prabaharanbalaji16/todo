2f52fa2645750451f66bdf935a53ba292b058098
prabaharanbalaji16 <prabaharanbalaji16@gmail.com>
2020-10-08 10:40:18 +0530

    Updates

---
+++ precache-manifest.68c98744a0d88cbb3c1f4c2543f5b100.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8b3b2cd0a9c7be04d29042f1e33a573",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "adeaa01596074261fe4d",
+    "url": "/todo/static/css/main.8c83ed8d.chunk.css"
+  },
+  {
+    "revision": "5d609804f16b3cee37d4",
+    "url": "/todo/static/js/2.4e4ac65e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.4e4ac65e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adeaa01596074261fe4d",
+    "url": "/todo/static/js/main.b1976226.chunk.js"
+  },
+  {
+    "revision": "d69eb0e01157d811ef11",
+    "url": "/todo/static/js/runtime-main.c30bcdbb.js"
+  }
+]);
